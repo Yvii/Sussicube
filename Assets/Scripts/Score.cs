83f257a53dfa1fa98eb5cb9d6afa2572fd3ce8df
Yvii <47932562+Yvii@users.noreply.github.com>
2019-03-01 09:26:51 +0100

    Update Score.cs
    Score always starts at 0. No matter where the Players startposition is

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -6,12 +6,22 @@ public class Score : MonoBehaviour
 
     public Transform player;
     public Text scoretext;
+    private float score = 0;
+    private float playerStartZPosition;
+
+    void Start()
+    {
+        // Find Player start posotion
+        playerStartZPosition = player.position.z;
+    }
 
 
     // Update is called once per frame
     void Update()
     {
-        scoretext.text = player.position.z.ToString("0");
+        //subtract current position from starting position to get the distance travelled 
+        score = player.position.z - playerStartZPosition;
+        scoretext.text = score.ToString("0");
     }
 
     public void ChangeTextToRed()
