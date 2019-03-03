cd9b896dfd3ab7c7684161bba33142b1ddd40d67
Yvii <47932562+Yvii@users.noreply.github.com>
2019-03-03 07:49:58 +0100

    New Level Complete UI

---
+++ Assets/Scripts/Level Complete UI/CompleteAttemptsText.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CompleteAttemptsText : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private Text txtAttempts;
+
+    void Start()
+    {
+        txtAttempts = GetComponent<Text>();
+        //Debug.Log(txtLevel.text);
+        if (GameMonitor.instance.isCurrentLevelaPlayableLevel == true && txtAttempts != null)
+        {
+            //txtLevel
+            txtAttempts.text = "ATTEMPTS: " + GameMonitor.instance.currentAttempts.ToString();
+        }
+    }
+}
---
+++ Assets/Scripts/Level Complete UI/CompleteLevelText.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CompleteLevelText : MonoBehaviour
+{
+
+    private Text txtLevel;
+
+    void Start()
+    {
+        txtLevel = GetComponent<Text>();
+        //Debug.Log(txtLevel.text);
+        if (GameMonitor.instance.isCurrentLevelaPlayableLevel == true && txtLevel != null)
+        {
+            //txtLevel
+            txtLevel.text = "LEVEL: " + GameMonitor.instance.currentLevel.ToString("D2");
+        }
+    }
+}
