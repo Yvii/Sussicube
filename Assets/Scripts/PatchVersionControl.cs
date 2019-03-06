429f6a4facd94c76ab2f4ed6b7ed4df029c2799f
Yvii <47932562+Yvii@users.noreply.github.com>
2019-03-06 22:13:48 +0100

    Added Basic Patching Notificationsystem

---
+++ Assets/Scripts/PatchVersionControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatchVersionControl : MonoBehaviour
+{
+    public string currentPatch = "0.3"; // Patch of the current Game
+    public GameObject patchPanel;       //Shows a Waring that a new Patch is available
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        string lastestPatch = RemoteSettings.GetString("currentPatch"); // Latest Version
+        Debug.Log("Remote Server: Newest Patch: " + lastestPatch);
+
+        if (currentPatch != lastestPatch)
+        {
+            patchPanel.SetActive(true);
+        }
+    }
+}
