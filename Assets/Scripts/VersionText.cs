a77676a90cf0698c02e187c24aa9746615895d1c
Yvii <47932562+Yvii@users.noreply.github.com>
2019-03-07 12:22:35 +0100

    New Version DebugText

---
+++ Assets/Scripts/VersionText.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//CURRENTLY ONLY USED FOR DEBUGGING
+
+public class VersionText : MonoBehaviour
+{
+
+    public Text txtversion;
+    public PatchVersionControl patchVersionControl;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        string lastestPatch = RemoteSettings.GetString("currentPatch"); // Latest Version
+        txtversion = GetComponent<Text>();
+        txtversion.text = lastestPatch + " " + patchVersionControl.GetCurrentPatch();
+
+        if (lastestPatch == patchVersionControl.GetCurrentPatch())
+        {
+            txtversion.color = Color.green;
+        }
+        else
+        {
+            txtversion.color = Color.red;
+        }
+    }
+
+}
