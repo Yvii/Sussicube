e25321e2586f280d3c333b653ef737155eceac38
Yvii <47932562+Yvii@users.noreply.github.com>
2019-04-17 05:33:58 +0200

    Added Modern UI

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -35,6 +35,7 @@ public class AudioManager : MonoBehaviour
             s.source.pitch = s.pitch;
             s.source.loop = s.loop;
             s.source.spatialBlend = s.spatialBlend;
+            s.source.outputAudioMixerGroup = s.audioMixerGroup;
         }
 
     }
--- Assets/Scripts/Sound.cs
+++ Assets/Scripts/Sound.cs
@@ -9,6 +9,8 @@ public class Sound
 
     public AudioClip audioClip;
 
+    public AudioMixerGroup audioMixerGroup;
+
     [Range(0f,1f)]
     public float volume;
     [Range(0.1f, 3f)]
